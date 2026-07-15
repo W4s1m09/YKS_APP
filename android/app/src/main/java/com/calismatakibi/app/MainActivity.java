4580788bce3dfbd9fd23eb26ca11fcdf5e8493e0
Wasim Zahra <wasimzahra2020@gmail.com>
2026-07-16 00:43:46 +0300

    first commit

---
+++ android/app/src/main/java/com/calismatakibi/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.calismatakibi.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.calismatakibi.app',
+  appName: 'Çalışma Takibi',
+  webDir: 'www'
+};
+
+export default config;
